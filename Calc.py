f64fdb301d310442198a3072ca1788d3722eb30f
camilof <camilo.andres.fernandez.gil@gmail.com>
2018-07-16 10:27:47 +0200

    testing python

---
+++ Calc.py
@@ -0,0 +1,19 @@
+class calculator:
+
+    def sum(x,y):
+        return x+y
+
+
+    def subtraction(x,y):
+        return x-y
+
+    def multiplication(x,y):
+        return x*y
+    def division(x,y):
+        return x/y
+
+
+
+x= calculator.division(100,2)
+
+print(x)
